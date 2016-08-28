73818e99a08832af754572d245e8ca7bf1ca43f8
pasca <pasca@DESKTOP-L19EOOB>
2016-08-28 02:57:25 +0200

    Pfeil eingebaut. Rotieren von Pfeil und Rose

--- ClickDummy/Scripts/spinner.js
+++ ClickDummy/Scripts/spinner.js
@@ -1,10 +1,21 @@
-﻿var s;
-var g;
+﻿var compassArea;
+var compassGroup;
+
+var arrowArea;
+var arrowGroup;
+
 window.onload = function () {
-    s = Snap(400,400);
-    g = s.group();
+    compassArea = Snap('#compassSvg');
+    compassGroup = compassArea.group();
     var compass = Snap.load("Kompass.svg", function (loadedFragment) {
-        g.append(loadedFragment);
+        compassGroup.append(loadedFragment);
+    });
+
+    arrowArea = Snap('#arrowSvg');
+    arrowGroup = arrowArea.group();
+    var arrow = Snap.load('Pfeil.svg', function (loadedFragement) {
+        arrowGroup.append(loadedFragement);
+        //arrowGroup.transform('t0,169');
     });
 }
 
@@ -12,6 +23,11 @@ function moveCompass(angle) {
     
     //var myMatrix = new Snap.Matrix();
     //myMatrix.rotate(45);
-    var bbox = g.getBBox(); //bounding box, get coords and centre
-    g.animate({ transform: "r180," + bbox.cx + ',' + bbox.cy }, 1000, mina.linear);
+    var bbox = compassGroup.getBBox(); //bounding box, get coords and centre
+    compassGroup.animate({ transform: 'r'+angle+',' + bbox.cx + ',' + bbox.cy }, 1000, mina.linear);
+}
+
+function moveAarrow(angle) {
+    var bbox = arrowGroup.getBBox();
+    arrowGroup.animate({ transform: 'r' + angle + ',' + bbox.cx + ',' + bbox.cy }, 1000, mina.linear)
 }
